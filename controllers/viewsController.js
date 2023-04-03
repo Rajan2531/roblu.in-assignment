00c9737db6869e81ad441389da59767b8805a69f
rajan chouhan <rajan.chouhan25@gmail.com>
2023-04-03 17:16:30 +0530

    changed width of courseDetail in style.css

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,10 +1,14 @@
 const Product=require('./../models/productModel.js');
+
+// controller function to inject product data into home page and render home page.
 exports.getHomePage=async(req,res,next)=>{
     const products=await Product.find();
 
     res.render('home',{products:products})
 }
 
+
+// controller function to render form for adding new product details
 exports.getAddProduct=(req,res,next)=>{
     res.render('addProduct');
 }
