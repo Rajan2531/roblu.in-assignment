f71edac54406d2183d1b328aa75c0c193f17ebfd
rajan chouhan <rajan.chouhan25@gmail.com>
2023-04-03 17:43:13 +0530

    updated bundle.js

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -12191,7 +12191,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63320" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63562" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
